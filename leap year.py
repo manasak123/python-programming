a98c6e0515822f37424ca1833c95e6edfa07b538
manasak123 <113346259+manasak123@users.noreply.github.com>
2022-09-12 15:51:50 +0530

    Create leap year.py

---
+++ leap year.py
@@ -0,0 +1,7 @@
+year=float(input("enter the year:"))
+if(year<=0):
+    print("invalid")
+elif(year%4==00):
+       print(" the year is leap year")
+else:
+      print("the year is not a leap year")
