4e203ff9f4853f83416104c5e99bb856b4b678c7
manasak123 <113346259+manasak123@users.noreply.github.com>
2022-09-15 22:31:42 +0530

    Create pattern*.py

---
+++ pattern*.py
@@ -0,0 +1,19 @@
+rows=int(input("enter the rows:"))
+for i in range(rows):
+   for j in range(1+i):
+     print("*",end=" ")
+   print("\n")
+
+  
+  output:
+    enter the rows:5
+* 
+
+* * 
+
+* * * 
+
+* * * * 
+
+* * * * * 
+
