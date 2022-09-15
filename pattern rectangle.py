650edc32b2eb6a0b6656afb3e9c08511579992cf
manasak123 <113346259+manasak123@users.noreply.github.com>
2022-09-15 22:56:50 +0530

    Create pattern rectangle.py

---
+++ pattern rectangle.py
@@ -0,0 +1,17 @@
+rows=int(input("enter the rows:"))
+size=5
+for i in range(size):
+    for j in range(size):
+        if i==0 or i==size-1 or j==0 or j==size-1:
+              print("*",end=" ")
+        else:
+            print(" ",end=" ")
+    print()
+
+    output:
+      enter the rows:5
+* * * * * 
+*       * 
+*       * 
+*       * 
+* * * * * 
